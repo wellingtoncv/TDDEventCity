e0d626e597929c1881d5f92c2fb11f48d10d85fa
Wellingtoncvs <wellington.santos@addasoftwares.com>
2024-02-06 21:38:37 -0300

    |Tests passaram, findAllShouldReturnAllResourcesSortedByName, insertShouldInsertResource, deleteShouldReturnNoContentWhenIndependentId e updateShouldUpdateResourceWhenIdExists

---
+++ src/main/java/com/devsuperior/demo/services/EventService.java
@@ -0,0 +1,33 @@
+package com.devsuperior.demo.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.devsuperior.demo.dto.EventDTO;
+import com.devsuperior.demo.entities.Event;
+import com.devsuperior.demo.repositories.EventRepository;
+import com.devsuperior.demo.services.exceptions.ResourceNotFoundException;
+
+import jakarta.persistence.EntityNotFoundException;
+
+@Service
+public class EventService {
+
+	@Autowired
+	private EventRepository repository;
+	
+
+	@Transactional
+	public EventDTO update(Long id, EventDTO dto) {
+		try {
+			Event entity = repository.getReferenceById(id);
+			//copyDtoToEntity(dto, entity);
+			entity = repository.save(entity);
+			return new EventDTO(entity);
+		} catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException("Id not found" + id);
+		}
+	}
+
+}
